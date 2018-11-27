d7cc20c56ef3d4d8fc4ef4f17a7e5fd88fd7c0b0
bjornlain <11500443@student.pxl.be>
2018-11-27 17:21:27 +0100

    Add project files.

---
+++ DevOpsNogGeenTeam/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DevOpsNogGeenTeam.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DevOpsNogGeenTeam/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace DevOpsNogGeenTeam.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
